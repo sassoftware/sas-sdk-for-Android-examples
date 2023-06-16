e58a65503cdf6f13cf218141d969727dd46accb6
Stephen Talley <stephentalley-sas@gmail.com>
2023-06-15 21:55:16 -0400

    Update SDK version to 30.9.0

--- PersonalizedApp/buildSrc/src/main/kotlin/Sas.kt
+++ PersonalizedApp/buildSrc/src/main/kotlin/Sas.kt
@@ -1,6 +1,6 @@
 object Sas {
     object sdk {
-        const val version = "30.8.0"
+        const val version = "30.9.0"
         const val groupId = "com.sas.android.visualanalytics"
         const val appbase = "$groupId:appbase:$version"
         const val toolkit = "$groupId:toolkit:$version"
